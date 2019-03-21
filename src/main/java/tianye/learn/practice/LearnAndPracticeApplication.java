9edd874ccba18d9d020567866c15d10c4f576ea7
litianye <tianye.li@tendcloud.com>
2019-03-21 15:04:35 +0800

    first commit

---
+++ src/main/java/tianye/learn/practice/LearnAndPracticeApplication.java
@@ -0,0 +1,13 @@
+package tianye.learn.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAndPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tianye/learn/practice/LearnAndPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package tianye.learn.practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnAndPracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
